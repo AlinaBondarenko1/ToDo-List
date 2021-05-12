85c2bf82aea5b237a12d0d0bf54106ca94918ac5
AlinaBondarenko1 <alinabondarenko37@gmail.com>
2021-05-12 21:50:49 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
+import ToDoItem from './Components/ToDoItem.jsx'
+import Header from './Components/Header.jsx'
+import ToDoForm from './Components/ToDoForm.jsx'
+import TodoList from './Components/TodoList.jsx'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      "id": 1,
+      "sequence": 1,
+      "title": "Task 1",
+      "completed": true
+    },
+    {
+      "completed": false,
+      "title": "Task 2",
+      "id": 2,
+      "sequence": 2
+    },
+    {
+      "completed": false,
+      "title": "Task 3",
+      "id": 3,
+      "sequence": 3
+    }
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className ="wrapper">
+      <Header />
+      {/* <div>
+        <ToDoForm /> 
+        <ToDoItem /> 
+        <ToDoItem /> 
+        <ToDoItem /> 
+      </div> */}
+
+  <TodoList todos={todos} />
+  </div>
   );
 }
 
